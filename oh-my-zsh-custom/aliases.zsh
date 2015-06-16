f93e712c626b81e95072c77a3e6fadd314bc6fd3
sbungartz <sbungartz@online.de>
2015-06-16 14:59:07 +0200

    keep gdc alias for git diff --cached

--- oh-my-zsh-custom/aliases.zsh
+++ oh-my-zsh-custom/aliases.zsh
@@ -4,5 +4,7 @@ alias gsir='git submodule update --init --recursive'
 alias gsu='git submodule update'
 alias gsur='git submodule update --recursive'
 
+alias gdc='git diff --cached'
+
 # ctags
 alias ctagsr='ctags -f .ctags -R --extra=+f .'
