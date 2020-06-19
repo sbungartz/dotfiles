6af1bb31d2c7d93498aafa3ab2cb01002fa05909
Simon Bungartz <simon.bungartz@zweitag.de>
2020-06-19 08:51:41 +0200

    Extend clearmode to run same command after each input

--- local-bundle/clear_mode.zsh
+++ local-bundle/clear_mode.zsh
@@ -4,16 +4,35 @@ function clearmode {
   case "$1" in
     on)
       CLEAR_MODE_ENABLED=true
+      unset CLEAR_MODE_COMMAND
       echo "Clearmode enabled"
       ;;
     off)
       CLEAR_MODE_ENABLED=false
+      unset CLEAR_MODE_COMMAND
       echo "Clearmode disabled"
       ;;
+    with)
+      CLEAR_MODE_ENABLED=true
+      shift;
+      CLEAR_MODE_COMMAND=$@
+      echo "Clearmode enabled. Running $CLEAR_MODE_COMMAND as default screen"
+      ;;
     *)
-      echo "Usage: '$0 [on, off]'"
+      echo "Usage: '$0 [on, off, with <command>]'"
       return 1
   esac 
+}
+
+function precmd {
+  if $CLEAR_MODE_ENABLED
+  then
+    if  [ ! -z "$CLEAR_MODE_COMMAND" ]
+    then
+      print -Pn "$PROMPT$CLEAR_MODE_COMMAND\n" | sed 's/32m➜/36m➜/'
+      eval $CLEAR_MODE_COMMAND
+    fi
+  fi
 }
 
 function preexec {
