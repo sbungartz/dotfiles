716dfe804781f1dfd4f53c44b9bc1c14575be98c
Simon Bungartz <simon.bungartz@zweitag.de>
2022-08-26 20:35:17 +0200

    Allow mouse drag with modifier

--- dotfiles/talon/user/mystuff/misc/mouse.py
+++ dotfiles/talon/user/mystuff/misc/mouse.py
@@ -1,11 +1,26 @@
-from talon import Module, ctrl
+from talon import Module, ctrl, actions
 
 mod = Module()
 
 @mod.action_class
 class Actions:
+  def mouse_drag(button: int, modifier: str = None):
+    """Press and hold/release a specific mouse button for dragging"""
+    # Clear any existing drags
+    actions.user.mouse_drag_end()
+
+    if modifier is not None:
+      actions.key(f"{modifier}:down")
+
+    # Start drag
+    ctrl.mouse_click(button=button, down=True)
+
   def mouse_drag_end():
     """Send mouse up event for any button that could be used with drag"""
     # Overriding default logic because talon.mouse_buttons_down() does not report correct numbers on Linux
     for button in [0, 1, 2]:
       ctrl.mouse_click(button, up=True)
+
+    # Also release any modifier keys
+    for modifier in ['ctrl', 'alt', 'super', 'shift']:
+      actions.key(f"{modifier}:up")
--- dotfiles/talon/user/mystuff/misc/mouse_custom.talon
+++ dotfiles/talon/user/mystuff/misc/mouse_custom.talon
@@ -10,3 +10,18 @@ mouse park:
 
 (mid|middle) drag: user.mouse_drag(2)
 drop: user.mouse_drag_end()
+
+<user.modifiers> (left drag | drag):
+	user.mouse_drag(0, "{modifiers}")
+	# close the mouse grid
+	user.grid_close()
+
+<user.modifiers> (right drag | righty drag):
+	user.mouse_drag(1, "{modifiers}")
+	# close the mouse grid
+	user.grid_close()
+
+<user.modifiers> (mid|middle) drag:
+	user.mouse_drag(2, "{modifiers}")
+	# close the mouse grid
+	user.grid_close()
