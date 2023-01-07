13ac0442308ce00aebd73c8ea766c7f05cb032ad
Simon Bungartz <simon.bungartz@zweitag.de>
2023-01-07 16:39:15 +0100

    Custom key names stolen from pokey_talon

--- dotfiles/talon/user/mystuff/misc/keys_custom_names.py
+++ dotfiles/talon/user/mystuff/misc/keys_custom_names.py
@@ -3,8 +3,46 @@ from talon import Module, Context
 mod = Module()
 ctx = Context()
 
-mod.list("mystuff_special_key", desc="alternate names for special keys")
+# Symbol keys
+mod.list("mystuff_symbol_key", desc="alternate names for symbol keys")
+ctx.lists["user.mystuff_symbol_key"] = {
+    "brick": "`",
+    "stroke": "/",
+    "backstroke": "\\",
+    "equal": "=",
+    "plus": "+",
+    "tilde": "~",
+    "bang": "!",
+    "score": "_",
+    "quest": "?",
+    "single": "'",
+    "double": '"',
+    "leper": "(",
+    "repper": ")",
+    "lacker": "[",
+    "racker": "]",
+    "lacer": "{",
+    "racer": "}",
+    "langle": "<",
+    "wrangle": ">",
+    "snow": "*",
+    "pound": "#",
+    "percy": "%",
+    "tangle": "^",
+    "amper": "&",
+    "pipe": "|",
+    "dollar": "$",
+    "semi": ";",
+    "stack": ":",
+    "drip": ",",
+}
 
+@ctx.capture("user.symbol_key", rule="{user.symbol_key} | {user.mystuff_symbol_key}")
+def symbol_key(m):
+    return str(m)
+
+# Special keys
+mod.list("mystuff_special_key", desc="alternate names for special keys")
 ctx.lists["user.mystuff_special_key"] = {
   "clap": "enter",
   "drill": "backspace",
